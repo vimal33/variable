18cda6b34ab250cbecad7f8ce91a9703ebd828ae
Vimal Raj .C <118258607+vimal33@users.noreply.github.com>
2022-12-05 21:57:20 +0530

    Add files via upload

---
+++ src/variable/Variable.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variable;
+
+/**
+ *
+ * @author Vimal
+ */
+public class Variable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number=10;
+        int number1=20;
+        int sum=number+number1;
+        if(sum>30)
+        {
+            System.out.println("the sum is greater than 30");
+        }
+    }
+   
+    
+}
---
+++ src/variable/display.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variable;
+
+/**
+ *
+ * @author Vimal
+ */
+public class display {
+    
+  String name;
+  static String qualification="B.E";
+  void message()
+  {
+      String name="vimal";
+      System.out.println("name");
+  }
+    
+}
